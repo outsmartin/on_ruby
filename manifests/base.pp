5947aa1fe3be8ff459fac0898c3b27e19b5c931b
Peter Schröder <phoetmail@googlemail.com>
2012-10-29 18:00:31 +0100

    install vim

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,9 +1,13 @@
 group { "puppet":
-  ensure => "present",
+  ensure => present,
 }
 
 File { owner => 0, group => 0, mode => 0644 }
 
-file { '/etc/motd':
-  content => "Welcome to your Vagrant-built virtual machine! Managed by Puppet.\n"
+file { "/etc/motd":
+  content => "Welcome to the OnRuby Virtual Box managed by Puppet.\n"
+}
+
+package { "vim":
+  ensure => present,
 }
